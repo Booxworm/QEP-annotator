0138d40e7c883bdc2aadb217bba17f8aed43bf72
Wonn Jen <lee.wonnjen@gmail.com>
2021-11-03 18:47:33 +0800

    Format login and add schema functiontionality

--- interface.py
+++ interface.py
@@ -5,57 +5,105 @@ from functools import partial
 
 from PIL import ImageTk, Image
 
+class BLabel(object):
+    b = "•"
+    def __init__(self,master):
+        import tkinter as tk
+        self.l = tk.Label(master)
+    def add_option(self,text):
+        if self.l.cget("text") == "":
+            self.l.config(text=self.b+" "+text)
+        else:
+            self.l.config(text=self.l.cget("text") +"\n"+ self.b + " "+text)
+
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.dbms = preprocessing.DBMS()
         self.qepAnnotator = annotate.QEPAnnotator()
         self.qepGraph = preprocessing.QepGraph()
-        self.loginWindow()
+
+        self.geometry("1200x800")
         self['background'] = '#62c2da'
 
+        self.loginWindow()
+
     def loginWindow(self):
-        #window
-        # tkWindow = tk.Tk()  
-        self.geometry('400x150')
         self.title('Tkinter Login Form')
 
+        title = tk.Label(self, text='Login')
+        title.pack(pady=50)
+        title['background'] = '#62c2da'
+        title.config(font=('Helvetica bold', 20, "bold"))
+
         topFrame = tk.Frame(self)
-        topFrame.pack()
+        topFrame.pack(pady=20)
+        topFrame['background'] = '#62c2da'
+
         bottomFrame = tk.Frame(self)
         bottomFrame.pack()
+        bottomFrame['background'] = '#62c2da'
 
         # database label and text entry box
-        databaseLabel = tk.Label(topFrame, text='Database').grid(row=0, column=0)
+        databaseLabel = tk.Label(topFrame, text='Database')
+        databaseLabel.grid(row=0, column=0)
+        databaseLabel.config(font=('Helvetica bold', 14, "bold"))
+        databaseLabel['background'] = '#62c2da'
         database = tk.StringVar(value='TPC-H')
-        databaseEntry = tk.Entry(topFrame, textvariable=database).grid(row=0, column=1)  
+        databaseEntry = tk.Entry(topFrame, textvariable=database)
+        databaseEntry.grid(row=0, column=1)
+        databaseEntry.config(font=('Helvetica bold', 14))
 
         # user label and text entry box
-        userLabel = tk.Label(topFrame, text='User').grid(row=1, column=0)
+        userLabel = tk.Label(topFrame, text='User')
+        userLabel.grid(row=1, column=0)
+        userLabel.config(font=('Helvetica bold', 14, "bold"))
+        userLabel['background'] = '#62c2da'
         user = tk.StringVar(value='postgres')
-        userEntry = tk.Entry(topFrame, textvariable=user).grid(row=1, column=1)  
+        userEntry = tk.Entry(topFrame, textvariable=user)
+        userEntry.grid(row=1, column=1)
+        userEntry.config(font=('Helvetica bold', 14))
 
         # password label and password entry box
-        passwordLabel = tk.Label(topFrame,text='Password').grid(row=2, column=0)  
+        passwordLabel = tk.Label(topFrame,text='Password')
+        passwordLabel.grid(row=2, column=0)
+        passwordLabel.config(font=('Helvetica bold', 14, "bold"))
+        passwordLabel['background'] = '#62c2da'
         password = tk.StringVar()
-        passwordEntry = tk.Entry(topFrame, textvariable=password, show='*').grid(row=2, column=1)  
+        passwordEntry = tk.Entry(topFrame, textvariable=password, show='*')
+        passwordEntry.grid(row=2, column=1)
+        passwordEntry.config(font=('Helvetica bold', 14))
 
         # host label and text entry box
-        hostLabel = tk.Label(topFrame, text='Host').grid(row=3, column=0)
+        hostLabel = tk.Label(topFrame, text='Host')
+        hostLabel.grid(row=3, column=0)
+        hostLabel.config(font=('Helvetica bold', 14, "bold"))
+        hostLabel['background'] = '#62c2da'
         host = tk.StringVar(value='localhost')
-        hostEntry = tk.Entry(topFrame, textvariable=host).grid(row=3, column=1)  
+        hostEntry = tk.Entry(topFrame, textvariable=host)
+        hostEntry.grid(row=3, column=1)
+        hostEntry.config(font=('Helvetica bold', 14))
 
         # port label and text entry box
-        portLabel = tk.Label(topFrame, text='Port').grid(row=4, column=0)
+        portLabel = tk.Label(topFrame, text='Port')
+        portLabel.grid(row=4, column=0)
+        portLabel.config(font=('Helvetica bold', 14, "bold"))
+        portLabel['background'] = '#62c2da'
         port = tk.StringVar(value='5432')
-        portEntry = tk.Entry(topFrame, textvariable=port).grid(row=4, column=1)  
+        portEntry = tk.Entry(topFrame, textvariable=port)
+        portEntry.grid(row=4, column=1)
+        portEntry.config(font=('Helvetica bold', 14))
 
         validateLogin = partial(self.validateLogin, database, user, password, host, port)
 
         #login button
-        loginButton = tk.Button(bottomFrame, text='Login', command=validateLogin).grid(row=0, column=0)
+        loginButton = tk.Button(bottomFrame, text='Login', command=validateLogin, foreground = 'white', background='#063970', height = 2, width = 20)
+        loginButton.grid(row=0, column=0)
+        loginButton.config(font=('Calibri', 14, "bold"))
         self.errorMsg = tk.Label(bottomFrame, text='Failed to connect to database', fg='#f00')
         self.errorMsg.grid(row=1, column=0)
+        self.errorMsg.config(font=('Helvetica bold', 14, "bold"))
+        self.errorMsg['background'] = '#62c2da'
         self.errorMsg.grid_forget()
 
     def validateLogin(self, database, user, password, host, port):
@@ -68,15 +116,11 @@ class App(tk.Tk):
         )
 
         if connected:
-            # self.errorMsg.grid_forget()
-            for widgets in self.winfo_children():
-                widgets.destroy()
-            self.createWindow()
+            self.goToMain()
         else:
             self.errorMsg.grid()
 
-    def createWindow(self):
-        self.geometry("1200x800")
+    def mainWindow(self):
         topFrame = tk.Frame(self)
         topFrame.pack()
         topFrame['background'] = '#62c2da'
@@ -85,9 +129,9 @@ class App(tk.Tk):
         bottomFrame.pack()
         bottomFrame['background'] = '#62c2da'
 
-        ############################
+        ##############################
         # Frame for Application Name #
-        ############################
+        ##############################
 
         # Frame widget for application name
         nameFrame = tk.Frame(master=topFrame)
@@ -159,14 +203,15 @@ class App(tk.Tk):
         annotationLabel['background'] = '#62c2da'
         annotationLabel.config(font=('Helvetica bold', 14, "bold"))
 
+        #############################
         # Frame for Database Schema #
-        ##############################
+        #############################
         schemaFrame = tk.Frame(master=bottomFrame)
         schemaFrame.pack(side=tk.LEFT, anchor='s')
         schemaFrame['background'] = '#62c2da'
 
         # Button widget to open database schema window
-        schemaButton = tk.Button(master=schemaFrame, text='Database Schema', foreground = 'white', height = 2, width = 20)
+        schemaButton = tk.Button(master=schemaFrame, text='Database Schema', command=self.goToSchema, foreground = 'white', height = 2, width = 20)
         schemaButton.pack(pady=33)
         schemaButton['background'] = '#063970'
         schemaButton.config(font=('Calibri', 14, "bold"))
@@ -190,8 +235,7 @@ class App(tk.Tk):
         self.img = ImageTk.PhotoImage(Image.open("images/blank.png"))  
         self.treeCanvas.create_image(0, 0, anchor=tk.NW, image=self.img)
 
-        # Invisible frame for alignment #
-        ##############################
+        # Invisible frame for alignment
         invisibleFrame = tk.Frame(master=bottomFrame)
         invisibleFrame.pack(side=tk.LEFT)
         invisibleFrame['background'] = '#62c2da'
@@ -201,8 +245,6 @@ class App(tk.Tk):
         invisibleLabel.pack(padx=30)
         invisibleLabel['background'] = '#62c2da'
 
-
-    
     def processQuery(self):
         # Get query
         query = ' '.join((self.queryText.get(1.0, tk.END)).split('\n'))
@@ -231,46 +273,131 @@ class App(tk.Tk):
         self.img = ImageTk.PhotoImage(Image.open("images/blank.png"))  
         self.treeCanvas.create_image(0, 0, anchor=tk.NW, image=self.img)
 
-# class QueryFrame(tk.Frame):
-#     def __init__(self, container):
-#         super().__init__(container)
+    def goToMain(self):
+        for widgets in self.winfo_children():
+            widgets.destroy()
+        self.mainWindow()
 
-#         # Label widget for query
-#         self.label = tk.Label(self, text='QUERY')
-#         self.label.pack()
+    def goToSchema(self):
+        for widgets in self.winfo_children():
+            widgets.destroy()
+        self.schemaWindow()
+    
+    def schemaWindow(self):
+        topFrame = tk.Frame(self)
+        topFrame.pack()
+        topFrame['background'] = '#62c2da'
 
-#         # Text widget to enter query
-#         self.text = tk.Text(self, height=10, width=30)
-#         self.text.pack()
+        bottomFrame = tk.Frame(self)
+        bottomFrame.pack()
+        bottomFrame['background'] = '#62c2da'
 
-#         # Button widget to submit query
-#         self.button = tk.Button(self, text='Submit')
-#         self.button.pack()
+        ###################
+        # Frame for title #
+        ###################
 
-# class AnnotationFrame(tk.Frame):
-#     def __init__(self, container):
-#         super().__init__(container)
+        # Frame widget for title
+        nameFrame = tk.Frame(master=topFrame)
+        nameFrame.pack(side=tk.TOP)
+        nameFrame['background'] = '#62c2da'
 
-#         # Label widget for annotation
-#         label = tk.Label(self, text='ANNOTATION')
-#         label.pack()
+        # Label widget for title
+        nameLabel = tk.Label(master=nameFrame, text = 'Database Schema')
+        nameLabel.config(font=('Helvetica bold', 20, "bold"))
+        nameLabel.pack()
+        nameLabel['background'] = '#62c2da'
 
-#         # Text widget to enter query
-#         text = tk.Text(self, state='disabled', height=10, width=30)
-#         text.pack()
 
-# class TreeFrame(tk.Frame):
-#     def __init__(self, container):
-#         super().__init__(container)
+        # ############################
+        # # Frame for entering query #
+        # ############################
 
-#         # Label widget for tree
-#         label = tk.Label(self, text='TREE')
-#         label.pack()
+        # # Frame widget for query
+        # queryFrame = tk.Frame(master=topFrame)
+        # queryFrame.pack(side=tk.LEFT)
+        # queryFrame['background'] = '#62c2da'
 
-#         # Canvas widget for image
-#         canvas = tk.Canvas(self, width=700, height=500)  
-#         canvas.pack()
+        # # Text widget to enter query
+        # self.queryText = tk.Text(master=queryFrame, width=40, height=10, borderwidth=4, relief="solid")
+        # self.queryText.pack(padx=20, pady=5)
 
-#         # Image of QEP tree
-#         self.img = ImageTk.PhotoImage(Image.open('images/blank.png'))  
-#         canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
+        # # Label widget for query
+        # queryLabel = tk.Label(master=queryFrame, text='Query')
+        # queryLabel.pack()
+        # queryLabel['background'] = '#62c2da'
+        # queryLabel.config(font=('Helvetica bold', 14, "bold"))
+
+        # #####################
+        # # Frame for buttons #
+        # #####################
+
+        # # Frame widget for buttons
+        # buttonFrame = tk.Frame(master=topFrame)
+        # buttonFrame.pack(side=tk.LEFT)
+        # buttonFrame['background'] = '#62c2da'
+    
+
+        # # Button widget to submit query
+        # submitQueryButton = tk.Button(master=buttonFrame, text='Input Query', command=self.processQuery, foreground = 'white', height = 2, width = 20)
+        # submitQueryButton.pack(pady=5)
+        # submitQueryButton['background'] = '#063970'
+        # submitQueryButton.config(font=('Calibri', 14, "bold"))
+
+        # # Button widget to clear query
+        # clearQueryButton = tk.Button(master=buttonFrame, text='Clear', command=self.clearQuery, foreground = 'white', height = 2, width = 20)
+        # clearQueryButton.pack(pady=5)
+        # clearQueryButton['background'] = '#063970'
+        # clearQueryButton.config(font=('Calibri', 14, "bold"))
+
+        # ###################################
+        # # Frame for displaying annotation #
+        # ###################################
+
+        # annotationFrame = tk.Frame(master=topFrame)
+        # annotationFrame.pack(side=tk.LEFT)
+        # annotationFrame['background'] = '#62c2da'
+
+        # # Text widget to enter query
+        # self.annotationText = tk.Text(master=annotationFrame, state=tk.DISABLED, width=40, height=10, borderwidth=4, relief="solid")
+        # self.annotationText.pack(padx=20, pady=5)
+
+        # annotationLabel = tk.Label(master=annotationFrame, text='Annotation')
+        # annotationLabel.pack()
+        # annotationLabel['background'] = '#62c2da'
+        # annotationLabel.config(font=('Helvetica bold', 14, "bold"))
+
+        #############################
+        # Frame for Database Schema #
+        #############################
+        schemaFrame = tk.Frame(master=bottomFrame)
+        schemaFrame.pack(side=tk.LEFT, anchor='s')
+        schemaFrame['background'] = '#62c2da'
+
+        # Button widget to open database schema window
+        schemaButton = tk.Button(master=schemaFrame, text='Back', command=self.goToMain, foreground = 'white', height = 2, width = 20)
+        schemaButton.pack(pady=33)
+        schemaButton['background'] = '#063970'
+        schemaButton.config(font=('Calibri', 14, "bold"))
+
+        #############################
+        # Frame for displaying tree #
+        #############################
+
+        schemaFrame = tk.Frame(master=bottomFrame)
+        schemaFrame.pack(side=tk.LEFT)
+        schemaFrame['background'] = '#62c2da'
+
+        schemaText = BLabel(master=schemaFrame)
+        schemaText.add_option('test1')
+        schemaText.add_option('test2')
+        schemaText.pack()
+
+        # Invisible frame for alignment
+        invisibleFrame = tk.Frame(master=bottomFrame)
+        invisibleFrame.pack(side=tk.LEFT)
+        invisibleFrame['background'] = '#62c2da'
+
+        # Invisible label to align treeFrame to center
+        invisibleLabel = tk.Label(master=invisibleFrame, text='', foreground = 'white', height = 2, width = 20)
+        invisibleLabel.pack(padx=30)
+        invisibleLabel['background'] = '#62c2da'
