6e2789aa8803c4210f9aceef139ca5a6dd4c17c8
kuroducky <kuroducky@gmail.com>
2021-11-10 19:27:59 +0800

    remove unnecessary files from docker and included updated requirements.txt

--- src/project.py
+++ src/project.py
@@ -1,4 +1,10 @@
 import interface
+import os
+
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 
 app = interface.App()
 app.mainloop()
